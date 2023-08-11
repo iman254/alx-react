8cd3f2da125d4210074e7f42b82cacfe88367c86
iman <stephanieiman254@gmail.com>
2023-08-11 15:32:04 +0300

    task 2 css file and image

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,36 +1,38 @@
-import { resolve } from "path";
+const path = require("path");
 
-export const mode = "production";
-export const entry = {
-  main: resolve(__dirname, "./js/dashboard_main.js"),
-};
-export const output = {
-  path: resolve(__dirname, "public"),
-  filename: "bundle.js",
-};
-export const performance = {
-  hints: false,
-  maxEntrypointSize: 512000,
-  maxAssetSize: 512000,
-};
-export const module = {
-  rules: [
-    {
-      test: /\.css$/i,
-      use: ["style-loader", "css-loader"],
-    },
-    {
-      test: /\.(gif|png|jp?g|svg)$/i,
-      use: [
-        "file-loader",
-        {
-          loader: "image-webpack-loader",
-          options: {
-            bypassOnDebug: true,
-            disable: true,
+module.exports = {
+  mode: "production",
+  entry: {
+    main: path.resolve(__dirname, "./js/dashboard_main.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(gif|png|jp?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
           },
-        },
-      ],
-    },
-  ],
+        ],
+      },
+    ],
+  },
 };
