4066f8a87c9fd7a337a24670d5cab1fd1806e304
iman <stephanieiman254@gmail.com>
2023-08-11 14:53:13 +0300

    task 2 css file and image

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+      main: path.resolve(__dirname, './js/dashboard_main.js'),
+    },
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: 'bundle.js',
+    }, 
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(gif|png|jp?g|svg)$/i,
+          use: [
+            "file-loader",
+            {
+              loader:"image-webpack-loader",
+              options: {
+                bypassOnDebug: true,
+                disable: true,
+             },
+            },
+          ],
+        },
+      ],
+    },
+     
+};
+
